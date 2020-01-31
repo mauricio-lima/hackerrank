01d276714842f858b5f028aa8a93d91664a2f371
Mauricio Lima <github@mauriciolima.eti.br>
2020-01-31 16:57:51 -0300

    Regex - Match one or more repetitions

---
+++ challenges/matching-one-or-more-repititions/solutions/match-one-or-more.js
@@ -0,0 +1,16 @@
+const Regex_Pattern = /^\d+[A-Z]+[a-z]+$/; 
+
+function processData(Test_String) {
+    console.log(!!Test_String.match(Regex_Pattern));
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
